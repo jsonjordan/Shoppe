ad863ebc71565dff3b51db91297c14caf357af64
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-05-26 17:50:41 -0400

    Solved second problem

--- stats.rb
+++ stats.rb
@@ -9,9 +9,13 @@ def file_path file_name
   File.expand_path "../data/#{file_name}.json", __FILE__
 end
 
-#* The user that made the most orders was __
 p = TransactionParser.new file_path("transactions")
+b = DataParser.new file_path("data")
 p.parse!
+b.parse!
+
+#* The user that made the most orders was __
+puts "Problem 1: The user that made the most orders was __"
 
 user_orders = {}
 user_orders.default = 0
@@ -26,3 +30,27 @@ end
 most_orders = largest_hash_key(user_orders)
 puts "User with most orders: #{most_orders.first}"
 puts "They had #{most_orders.last} orders"
+
+#* We sold __ Ergonomic Rubber Lamps
+puts
+puts "Problem 2: We sold __ Ergonomic Rubber Lamps"
+
+erl_id = 0
+b.items.each do |i|
+  if i.name == "Ergonomic Rubber Lamp"
+    erl_id = i.id
+  end
+end
+puts "The Ergonomic Rubber Lamps id number is #{erl_id}"
+
+erls_sold = 0
+p.transaction.each do |t|
+  if t.item_id == erl_id
+    erls_sold += t.quantity
+  end
+end
+puts "We sold #{erls_sold} Ergonomic Rubber Lamps"
+
+#* We sold __ items from the Tools category
+puts
+puts "Problem 3: We sold __ items from the Tools category"
